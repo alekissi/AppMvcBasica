c75a50b6434bd5dcf3a8b5de85aa76b0113ed30f
Alex Moura <alekissi@gmail.com>
2023-07-28 21:30:24 -0300

    commit inicial

---
+++ AppMvcBasica/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMvcBasica.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> produtos { get; set; }
+        public DbSet<Fornecedor> fornecedores { get; set; }
+        public DbSet<Endereco> enderecos { get; set; }
+
+
+    }
+}
---
+++ AppMvcBasica/Data/Migrations/20230721133536_Tabelas-Sistema.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppMvcBasica.Data.Migrations
+{
+    public partial class TabelasSistema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fornecedores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Documento = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    TipoFornecedor = table.Column<int>(type: "int", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fornecedores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "enderecos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FornecedorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Logradouro = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Numero = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cep = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_enderecos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_enderecos_fornecedores_FornecedorId",
+                        column: x => x.FornecedorId,
+                        principalTable: "fornecedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FornecedorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_produtos_fornecedores_FornecedorId",
+                        column: x => x.FornecedorId,
+                        principalTable: "fornecedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_enderecos_FornecedorId",
+                table: "enderecos",
+                column: "FornecedorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_produtos_FornecedorId",
+                table: "produtos",
+                column: "FornecedorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "enderecos");
+
+            migrationBuilder.DropTable(
+                name: "produtos");
+
+            migrationBuilder.DropTable(
+                name: "fornecedores");
+        }
+    }
+}
